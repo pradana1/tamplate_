50b9e25052cab9d41b2166d46ac7a575d63fc463
unknown <pradanayp19@gmail.com>
2021-06-09 00:12:23 +0700

    alert

---
+++ cishop/application/views/layouts/_alert.php
@@ -0,0 +1,38 @@
+<?php
+
+$success    = $this->session->flashdata('success');
+$error      = $this->session->flashdata('error');
+$warning    = $this->session->flashdata('warning');
+
+if ($success) {
+    $alert_status   = 'alert-success';
+    $status         = 'success!';
+    $message        = $success;
+}
+
+if ($error) {
+    $alert_status   = 'alert-danger';
+    $status         = 'error!';
+    $message        = $error;
+}
+
+if ($warning) {
+    $alert_status   = 'alert-warning';
+    $status         = 'warning!';
+    $message        = $warning;
+}
+
+?>
+
+<?php if ($success || $error || $warning) :   ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert <?= $alert_status ?> alert-dismissible fade show" role="alert">
+                <strong><?= $status ?></strong> <?= $message ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+<?php endif ?>
